60fa903bdddb1cd9ef19fba36721fbec178b99e9
Alexandre Canivez <alexandre.canivez@gmail.com>
2018-07-25 14:07:03 +0200

    add basic test

--- tests/Controller/MyControllerTest.php
+++ tests/Controller/MyControllerTest.php
@@ -2,22 +2,40 @@
 
 namespace App\Tests\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Panther\PantherTestCase;
 
-class MyControllerTest extends WebTestCase
+class MyControllerTest extends PantherTestCase
 {
     public function testMyAction()
     {
-        $client = static::createClient();
+        $client = static::createPantherClient();
 
         $client->request('GET', '/');
 
         $crawler = $client->getCrawler();
+
+        $client->takeScreenshot('screen.jpg');
+        sleep(5);
+
         $this->assertContains(
             'Symfony 4.1.2',
             $client->getResponse()->getContent()
         );
 
         $this->assertEquals(1, $crawler->filter('h1')->count());
+    }
+
+    public function testDocAction()
+    {
+        $client = static::createPantherClient();
+
+        $crawler = $client->request('GET', '/');
+        sleep(2);
+
+        $link = $crawler->selectLink('How to create your first page in Symfony')->link();
+        $crawler = $client->click($link);
+
+        sleep(2);
+        $this->assertEquals('Create your First Page in Symfony', $crawler->filter('h1')->text());
     }
 }
